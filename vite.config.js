5d697b344493111f38a3b84d67c208930fce14ef
Arvaa11 <arvaamehmood11@gmail.com>
2026-09-10 07:45:23 -0700

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Buildcv/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/Buildcv/',
 })
